f62b99084e9101ee90f94a4fd48f26ae04130287
Shamiur Rahman <TechSR101@gmail.com>
2019-12-11 21:42:18 +0400

    2> 3. Connecting To MongoDB With Mongoose

---
+++ config/db.js
@@ -0,0 +1,22 @@
+//all MongoDB connections related
+
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true, // to Avoid DeprecationWarning
+      useNewUrlParser: true // to Avoid DeprecationWarning
+    }); // Because mongoose.connect return a promise we wanna put await
+
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.log(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
