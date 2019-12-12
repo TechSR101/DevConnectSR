ceddc7e189cd0b974a263df455955043e6112d8a
Shamiur Rahman <TechSR101@gmail.com>
2019-12-12 18:08:05 +0400

    4> 2. Get Current User Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+// const User = require("../../models/User");
 
-// @route           GET api/profile
-// @description     Test route
-// @access          Public
+// @route           GET api/profile/me
+// @description     Get Current User Profile
+// @access          Private
 
-router.get("/", (req, res) => res.send("Profile route"));
+// whatever route we want to protect we just use auth as 2nd parameter.
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
