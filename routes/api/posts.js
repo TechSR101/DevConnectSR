344d4ffa773369f7a8fa97fef0b56443853bf6b6
Shamiur Rahman <TechSR101@gmail.com>
2019-12-13 19:49:47 +0400

    5> 3. Get & Delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 const auth = require("../../middleware/auth");
 
 const User = require("../../models/User");
@@ -44,4 +44,64 @@ router.post(
   }
 );
 
+// @route           GET api/posts
+// @description     Get all posts
+// @access          Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const post = await Post.find().sort({ date: -1 });
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(400).send("Server Error");
+  }
+});
+
+// @route           GET api/posts
+// @description     Get post by ID
+// @access          Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post Not Found " });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post Not Found " });
+    }
+    res.status(400).send("Server Error");
+  }
+});
+
+// @route           DELETE api/posts/:id
+// @description     Delete a post
+// @access          Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: "Post Not Found " });
+    }
+
+    //Check if the Owner is deleting the post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Usr not Authorized " });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post Removed " });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post Not Found " });
+    }
+    res.status(400).send("Server Error");
+  }
+});
 module.exports = router;
