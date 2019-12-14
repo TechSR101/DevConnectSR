14d10d9934a7caf500ccd606104d60237688b0ef
Shamiur Rahman <TechSR101@gmail.com>
2019-12-14 21:42:57 +0400

    7> 2. Creating a Redux Store

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,28 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
-
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 
+//Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route path="/" exact component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route path="/register" exact component={Register} />
-          <Route path="/login" exact component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route path="/" exact component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route path="/register" exact component={Register} />
+            <Route path="/login" exact component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+//rootReducer
+
+import { combineReducers } from "redux";
+
+export default combineReducers({});
